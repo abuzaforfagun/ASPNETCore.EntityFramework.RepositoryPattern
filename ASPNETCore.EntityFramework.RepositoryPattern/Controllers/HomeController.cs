e54e219f605a509006534f1e732eb6d412cc4558
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-11-15 02:01:36 +0600

    Remove unnecessary codes

--- ASPNETCore.EntityFramework.RepositoryPattern/Controllers/HomeController.cs
+++ ASPNETCore.EntityFramework.RepositoryPattern/Controllers/HomeController.cs
@@ -29,24 +29,5 @@ namespace ASPNETCore.EntityFramework.RepositoryPattern.Controllers
             unitOfWorkRepository.Complete();
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
